9df414a482af8c5efb756a51050998bfb185482d
Akif Grape <121309672+akifgrape@users.noreply.github.com>
2023-04-09 22:11:56 +0300

    Update emty.cpp

--- emty.cpp
+++ emty.cpp
@@ -12350,3 +12350,583 @@ int main() {
 }#include <iostream>
 using namespace std;
 
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+vint main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "E   I   X" << endl;
+   cout << "  W   A   L   L   E   T" << endl;
+   return 0;
+}#include <iostream>
+using namespace std;
